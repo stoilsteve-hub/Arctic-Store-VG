015ae6a659a8bb41f61ed6a3a795daaca0ffd605
stoilsteve-hub <stoil.steve@me.com>
2026-03-22 16:22:03 +0100

    Fetch single product data from API

--- product.js
+++ product.js
@@ -1,3 +1,13 @@
 // product.js
 const urlParams = new URLSearchParams(window.location.search);
 const productId = urlParams.get('id');
+
+async function getProduct() {
+    const res = await fetch('https://fakestoreapi.com/products/' + productId);
+    const product = await res.json();
+    console.log(product);
+}
+
+if (productId) {
+    getProduct();
+}
